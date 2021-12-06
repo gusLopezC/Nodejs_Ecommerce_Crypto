db0c90e2c9ed00fb8540e5c266e9ece1df5a1039
gusLopezC <guslopezcallejas@gmail.com>
2021-12-06 03:21:09 -0600

    Settings for heroku

--- src/index.js
+++ src/index.js
@@ -77,5 +77,7 @@ app.get("/charge-cancel", (req, res) => {
   res.send("Payment cancel");
 });
 
-app.listen(3000);
-console.log("Server on port", 3000);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
+console.log("Server on port", PORT);
